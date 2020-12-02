b889c7e13fb9da13c7a9d645ed221e5776032617
Ankita Nigam <ankitanigam51@gmail.com>
2020-12-02 20:27:59 +0530

    checking

--- src/index.js
+++ src/index.js
@@ -7,12 +7,50 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get('/handle', (req,res) => {
+    console.log("Hello world!");
+});
 
+let num1 = 10;
+let num2 = 5;
+
+app.post('/add', (req,res) => {
+    res.send({
+        status: "success "|" failure "|" error",
+        message: "the sum of given two numbers",
+        sum: num1+num2
+        })
+})
+
+app.post('/sub', (req,res) => {
+    res.send({
+        status: "success"|"failure"|"error",
+        message: "the difference of given two numbers",
+        difference: num1-num2
+        })
+})
+
+app.post('/multiply', (req,res) => {
+    res.send({
+        status: "success"|"failure"|"error",
+        message: "The product of given numbers",
+        result: num1*num2
+        })
+})
+
+app.post('/divide',(req,res) => {
+    let num2 = 0;
+    res.status(401).send({
+        status: "success"|"failure"|"error",
+        message: "The division of given numbers",
+        result: num1/num2
+        })
+        console.log("Cannot divide by zero");
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
